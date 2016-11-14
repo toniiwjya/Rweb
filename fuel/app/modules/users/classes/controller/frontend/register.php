03ff3feda678a3746b008afa1ff505176b5c2581
Toni Wijaya <toni.microad@gmail.com>
2016-11-14 21:41:35 +0700

    Register User Function & Validation

--- fuel/app/modules/users/classes/controller/frontend/register.php
+++ fuel/app/modules/users/classes/controller/frontend/register.php
@@ -4,19 +4,30 @@ namespace Users;
 class Controller_Frontend_Register extends \Controller_Frontend
 {
     public function action_index(){
+        if(empty($this->_data_template)){
+            $this->_data_template=[];
+        }
     	$this->_do_registration();
-        return \Response::forge(\View::forge('users::frontend/register.twig'));
+        return \Response::forge(\View::forge('users::frontend/register.twig',$this->_data_template,FALSE));
     }
 
     private function _do_registration(){
     	$_post_data = \Input::post();
-
     	if (count($_post_data) > 0) {
     		$this->_data_template['post_data'] = $_post_data;
-
-    		$member = Model_Register::fill($_post_data);
-    		\Response::redirect(\Uri::base().'login');
+            $_err = Model_Register::form_validate();
+            if(count($_err) > 0){
+                $this->_data_template['error_message'] = $_err;
+                return;
+            }
+            try{
+                $member = Model_Register::fill($_post_data);
+                \Response::redirect(\Uri::base().'login');  
+            }
+            catch (\Exception $e){
+                $this->_data_template['error_message'] = $e -> getMessage();
+            }
     	}
-
     }
+
  }
--- fuel/app/modules/users/classes/model/register.php
+++ fuel/app/modules/users/classes/model/register.php
@@ -21,6 +21,12 @@ class Model_Register extends \Orm\Model {
 		)
 	);
 
+	private static $_label = [
+        'fName' => 'Full Name',
+		'email' => 'Email',
+		'password' => 'Password'
+    ];
+
 	protected static $_properties = array(
 		'id',
 		'fName' => array(
@@ -45,6 +51,8 @@ class Model_Register extends \Orm\Model {
 		),
 	);
 
+	
+
 	public static function fill($post_data) {
 		$member = Model_Register::forge(array(
 			'fName' => $post_data['fName'],
@@ -53,6 +61,20 @@ class Model_Register extends \Orm\Model {
 		));
 
 		$member->save();
+	}
+
+	public static function form_validate(){
+		$val = \Validation::forge('form_registration');
+		$val->add_field('fName','Full Name','required');
+		$val->add_field('email','Email','required');
+		$val->add_field('password','Password','required|min_length[6]');
+		$val->set_message('required','Field :label is required!');
+		$val->set_message('min_length','Field :label require minimum character!');
+		if($val->run()){
+			return[];
+		} else {
+			return $val->error();
+		}
 	}	
 
 }
