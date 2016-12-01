882c23ed844740ef744ab7ad60d466fdfd7bf0f3
Monika Yuliana <monika.microad@gmail.com>
2016-12-01 10:25:01 +0700

    news page & footer

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=rweb',
+			'dsn'        => 'mysql:host=localhost;dbname=reward',
 			'username'   => 'root',
 			'password'   => '',
 		),
